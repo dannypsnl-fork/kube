c8fd8a9862486289f900933ffcc62bce16d0c48a
dannypsnl <dannypsnl@gmail.com>
2019-07-21 16:28:03 +0800

    format

--- build.rs
+++ build.rs
@@ -1,9 +1,18 @@
 use std::process::Command;
 
 fn main() {
-	// build Go c-shared library
-	Command::new("go").args(&["build", "-o", "libkube_wrap.so", "-buildmode=c-shared", "main.go"]).status().unwrap();
+    // build Go c-shared library
+    Command::new("go")
+        .args(&[
+            "build",
+            "-o",
+            "libkube_wrap.so",
+            "-buildmode=c-shared",
+            "main.go",
+        ])
+        .status()
+        .unwrap();
 
-	println!("cargo:rustc-link-lib=dylib=kube_wrap");
-	println!("cargo:rustc-link-search=.");
+    println!("cargo:rustc-link-lib=dylib=kube_wrap");
+    println!("cargo:rustc-link-search=native=.");
 }
